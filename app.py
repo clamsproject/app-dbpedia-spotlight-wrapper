c62b9367ae23cfce47cd6fb526f52e523f3e3981
Keigh Rim <keigh.rim@gmail.com>
2023-07-24 04:48:34 -0400

    updated to sdk 1.0.9

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import time
 
 from bs4 import BeautifulSoup
 from lapps.discriminators import Uri
+import logging
 import re
 import requests
 from requests.adapters import HTTPAdapter
@@ -36,6 +37,7 @@ class DbpediaWrapper(ClamsApp):
 
     def _annotate(self, mmif: Union[str, dict, Mmif], **parameters) -> Mmif:
         # see https://sdk.clams.ai/autodoc/clams.app.html#clams.app.ClamsApp._annotate
+        self.logger.debug(f"Parameters: {parameters}")
 
         def _get_qid(uri: str) -> List[str]:
             """
@@ -84,6 +86,7 @@ class DbpediaWrapper(ClamsApp):
             if 'Resources' not in json_body:
                 raise Exception("No resources found in Spotlight response.")
             for resource in json_body['Resources']:
+                self.logger.debug(f"Found resource {resource['@surfaceForm']}, {resource['@URI']}")
                 named_ents[resource['@surfaceForm']] = dict(text=resource['@surfaceForm'],
                                                             offset_start=resource['@offset'],
                                                             offset_end=int(resource['@offset']) + len(
@@ -105,15 +108,20 @@ class DbpediaWrapper(ClamsApp):
             return named_ents
 
         # ensure that server is ready
+        self.logger.debug("Waiting for server to be ready...")
         self.session.get(self.address)
         if not isinstance(mmif, Mmif):
             mmif: Mmif = Mmif(mmif)
         for doc in mmif.get_documents_by_type(DocumentTypes.TextDocument):
+            self.logger.debug(f"Processing document {doc.id} @ {doc.location_path()}")
             res_json = _post_request(doc.text_value, **parameters)
+            self.logger.debug(f"Response: {res_json.keys()}")
             entities = _get_ne_links(res_json)
+            self.logger.debug(f"Found {len(entities)} entities.")
             did = f"{doc.parent}:{doc.id}" if doc.parent else doc.id
             new_view = mmif.new_view()
-            self.sign_view(new_view)
+            self.logger.debug(f"Created new view {new_view.id} and siging with {parameters}")
+            self.sign_view(new_view, parameters)
             new_view.new_contain(Uri.NE, document=did)
             for e in entities:
                 anno = new_view.new_annotation(Uri.NE)
@@ -122,6 +130,7 @@ class DbpediaWrapper(ClamsApp):
                 anno.add_property('category', entities[e]['category'])
                 anno.add_property('text', entities[e]['text'])
                 anno.add_property('grounding', entities[e]['grounding'])
+                self.logger.debug(f"Created annotation {anno.id} with grounding {entities[e]['grounding']}")
         return mmif
 
 
@@ -144,9 +153,7 @@ def test(infile, outfile) -> None:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--port", action="store", default="5000", help="set port to listen"
-    )
+    parser.add_argument("--port", action="store", default="5000", help="set port to listen")
     parser.add_argument("--production", action="store_true", help="run gunicorn server")
     parser.add_argument("-a", "--address", default="localhost:2222", help="set listening address for spotlight server")
     parser.add_argument("-t", "--test", action='store_true', help="bypass the server")
@@ -161,9 +168,11 @@ if __name__ == "__main__":
         # create the app instance
         app = DbpediaWrapper(address=parsed_args.address)
 
-        http_app = Restifier(app, port=int(parsed_args.port)
-                             )
+        http_app = Restifier(app, port=int(parsed_args.port))
+        # for running the application in production mode
         if parsed_args.production:
             http_app.serve_production()
+        # development mode
         else:
+            app.logger.setLevel(logging.DEBUG)
             http_app.run()
